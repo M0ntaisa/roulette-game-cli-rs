99c7ef28d270921a3b94e7ad0054034f051b2079
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-01-07 10:46:42 +0800

    change the game rule

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,58 @@
-use rand::Rng;
+use rand::{seq::SliceRandom, Rng};
+
+fn get_user_bet() -> i32 {
+    println!("Enter your bet:");
+    let mut bet = String::new();
+    std::io::stdin().read_line(&mut bet).expect("Failed to read line");
+    let bet: i32 = match bet.trim().parse() {
+      Ok(num) => num,
+      Err(_) => {
+        println!("Invalid input. Please enter a number.");
+        get_user_bet()
+      }
+    };
+    bet
+}
+
+fn shuffle(roulette_wheel: &mut [i32]) {
+    let mut rng = rand::thread_rng();
+    roulette_wheel.shuffle(&mut rng);
+}
+  
+fn random_number_between(min: usize, max: usize) -> usize {
+    rand::thread_rng().gen_range(min..max)
+}
+
+fn pay_out(amount: i32) {
+    println!("You won ${}!", amount);
+}
 
 fn main() {
     println!("Welcome to the roulette table!");
 
-    // Generate a random number between 0 and 36
-    let winning_number = rand::thread_rng().gen_range(0..37);
+    let mut roulette_wheel: &mut [i32] = &mut [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 5, 5, 5, 5, 10, 10, 20];
 
-    println!("The winning number is: {}", winning_number);
+    loop {
+        // shuffle the numbers in the roulette_wheel array
+        shuffle(&mut roulette_wheel);
 
-    println!("Enter your bet (1-36 for a specific number, 37 for red, 38 for black):");
+        // let the user place their bet
+        let bet = get_user_bet();
 
-    let mut bet = String::new();
-    std::io::stdin().read_line(&mut bet).expect("Failed to read line");
-
-    let bet: u32 = bet.trim().parse().expect("Please enter a valid number");
-
-    // Check if the bet is a specific number, red, or black
-    if bet == winning_number {
-        println!("Congratulations, you win!");
-    } else if bet == 37 {
-        if winning_number % 2 == 0 {
-            println!("Sorry, you lose. The winning number was black.");
+        // spin the roulette wheel
+        let spun_number = roulette_wheel[random_number_between(0, roulette_wheel.len() - 1)];
+        println!("{}", &spun_number);
+        // check if the user won
+        if spun_number == bet {
+            // multiply the user's bet by the number they bet on
+            let winnings = bet * roulette_wheel[bet as usize];
+            // pay out the winnings to the user
+            pay_out(winnings);
+            break;
         } else {
-            println!("Congratulations, you win! The winning number was red.");
+            // the user lost, so don't pay out anything
+            println!("Sorry, you lost. Better luck next time!")
         }
-    } else if bet == 38 {
-        if winning_number % 2 == 0 {
-            println!("Congratulations, you win! The winning number was black.");
-        } else {
-            println!("Sorry, you lose. The winning number was red.");
-        }
-    } else {
-        println!("Sorry, you lose. The winning number was {}.", winning_number);
     }
 }
 
